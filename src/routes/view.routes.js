a126c06e775aa1dd75274c12274ad486c5dd59f5
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-02-01 19:48:54 +0530

    fix: correct JWT middleware import name in view routes

--- src/routes/view.routes.js
+++ src/routes/view.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { addVideoView, getVideoViews, removeView } from "../controllers/view.controller.js"
-import {verifyJwt} from "../middlewares/auth.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router=Router();
-router.use(verifyJwt);
+router.use(verifyJWT);
 
 router.route("/:videoId")
             .get(getVideoViews)
